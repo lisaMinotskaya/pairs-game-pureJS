4795d38bf3f5afbc1e597c8be29e3e95113adf8b
Lisa Minotskaya <minotskaya-lisa@mail.ru>
2024-01-15 10:01:27 +0300

    added game-board .html and .css

--- main.js
+++ main.js
@@ -23,17 +23,39 @@ function shuffle(arr) {
 
 console.log(shuffle(array))
 
-function createGameBoard() {
-    const gameBoard = document.createElement('div')
-    const gameBoardContent = document.createElement('div')
-    const gameBoardHeader = document.createElement('')
+function createCard(number) {
+    // const cardsWrapper = document.createElement('div')
+    let card = document.createElement('div')
+    let cardFaceBack = document.createElement('div')
+    let cardFaceFront = document.createElement('div')
+
+    // cardsWrapper.classList.add('cards-container')
+    card.classList.add('card')
+    cardFaceBack.classList.add('card-face', 'number')
+    cardFaceFront.classList.add('card-face', 'cover')
+
+    cardFaceBack.textContent = number
+
+    card.append(cardFaceFront, cardFaceBack)
+
+    return card
 }
 
-function createCard() {
-    const buttonWrapper = document.createElement('div')
-    const button = document.createElement('button')
+let cardsContainer = document.querySelector(".cards-container")
+
+for (let element of array) {
+    let item = createCard(element.toString())
+    cardsContainer.append(item)
+}
+
+const cards = document.querySelectorAll(".card")
+
+function flipCard() {
+    this.classList.toggle('flip')
 
 }
+  
+cards.forEach(card => card.addEventListener('click', flipCard))
 
 // Этап 3. Используйте две созданные функции для создания массива перемешанными номерами. На основе этого массива вы можете создать DOM-элементы карточек. У каждой карточки будет свой номер из массива произвольных чисел. Вы также можете создать для этого специальную функцию. count - количество пар.
 
